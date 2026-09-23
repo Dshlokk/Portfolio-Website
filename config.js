cf10e13760e4762413b00a6189be945bf83963fe
Dshlokk <divyashlokk@gmail.com>
2026-09-23 18:16:48 +0530

    Video added

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Central Configuration File for Portfolio Website
 const CONFIG = {
     // Replace 'your_project_id' with your actual Sanity project ID
-    SANITY_PROJECT_ID: 'k7cx53x9,
+    SANITY_PROJECT_ID: 'k7cx53x9',
     
     // Replace 'production' with your Sanity dataset name if different
     SANITY_DATASET: 'production'
